b5fe18e060ccafa0f81901705d7d412a794f7226
fly-1-1 <2673024411@qq.com>
2023-11-10 01:36:00 +0800

    实现哈希

--- day1/demo/main.go
+++ day1/demo/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 // Block 定义结构
 type Block struct {
@@ -9,22 +12,33 @@ type Block struct {
 	//当前区块哈希
 	Hash []byte
 	//数据
-	Date []byte
+	Data []byte
 }
 
 // NewBlock 创建区块
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
 		PrevHash: prevBlockHash,
-		Hash:     []byte{}, //TODO
-		Date:     []byte(data),
+		Hash:     []byte{},
+		Data:     []byte(data),
 	}
+	block.SetHash()
+
 	return &block
+}
+
+// SetHash 生成哈希
+func (block *Block) SetHash() {
+	//1 拼装数据
+	blockInfo := append(block.PrevHash, block.Data...)
+	//2 sha256
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
 }
 
 func main() {
 	block := NewBlock("A->B 1 BTC", []byte{})
 	fmt.Printf("前区块Hash:%x\n", block.PrevHash)
 	fmt.Printf("当前区块Hash:%x\n", block.Hash)
-	fmt.Printf("区块数据:%s\n", block.Date)
+	fmt.Printf("区块数据:%s\n", block.Data)
 }
