0c0849c89f7c25690e0978b1eec80862a8d7f6f3
fly-1-1 <2673024411@qq.com>
2023-11-11 05:02:12 +0800

    测试函数

--- day1/demo/main.go
+++ day1/demo/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
@@ -11,11 +9,17 @@ func main() {
 	bc := NewBlockChain()
 	bc.AddBlock("A->B 20 BTC")
 	bc.AddBlock("A->B 30 BTC")
-	for i, block := range bc.blocks {
-		fmt.Printf("前区块height:%d\n", i)
+	it := bc.NewIterator()
+	//调用迭代器 返回每一个区块数据
+	for {
+		//返回区块 左移
+		block := it.Next()
+		fmt.Printf("=========================================\n\n")
 		fmt.Printf("前区块Hash:%x\n", block.PrevHash)
 		fmt.Printf("当前区块Hash:%x\n", block.Hash)
 		fmt.Printf("区块数据:%s\n", block.Data)
+		if len(block.PrevHash) == 0 {
+			break
+		}
 	}
-
 }
