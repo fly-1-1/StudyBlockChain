3bc40c3b1e7918fd04a962eb6ec5cc0012eccce3
fly-1-1 <2673024411@qq.com>
2023-11-12 22:01:46 +0800

    新增命令

--- day1/demo/cil.go
+++ day1/demo/cil.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 //接收命令行参数&控制区块链操作的文件
@@ -14,9 +15,16 @@ type CLI struct {
 const Usage = `
 	addBlock --data DATA  "add data to blockchain"
 	printChain			  "print all blockchain data"
+	printChainR			  "print all blockchain data revers"
+	getBalance --address ADDRESS      "get balance of address"
+	send FROM TO AMOUNT MINER DATA    "由FROM转AMOUNT给TO,由MINER挖矿,同时写入DATA"
 `
+
 const addCil = "addBlock"
 const printCil = "printChain"
+const rprintCil = "printChainR"
+const getbalaceCil = "getBalance"
+const sendCil = "send"
 
 // Run 接收参数的动作
 func (cil *CLI) Run() {
@@ -42,8 +50,30 @@ func (cil *CLI) Run() {
 			fmt.Printf(Usage)
 		}
 	case printCil:
-		fmt.Print("打印区块\n")
-		cil.PrintBlockChain()
+		fmt.Printf("正向打印区块\n")
+		cil.PrintChain()
+	case rprintCil:
+		fmt.Printf("反向打印区块\n")
+		cil.PrintBlockChainRevers()
+	case getbalaceCil:
+		fmt.Printf("获取余额\n")
+		if len(args) == 4 && args[2] == "--address" {
+			address := args[3]
+			cil.GetBalance(address)
+		}
+	case sendCil:
+		fmt.Printf("转账开始...\n")
+		if len(args) != 7 {
+			fmt.Println("参数个数错误")
+			fmt.Printf(Usage)
+			return
+		}
+		from := args[2]
+		to := args[3]
+		amount, _ := strconv.ParseFloat(args[4], 64)
+		miner := args[5]
+		data := args[6]
+		cil.Send(from, to, amount, miner, data)
 	default:
 		fmt.Print("无效的命令\n")
 		fmt.Printf(Usage)
