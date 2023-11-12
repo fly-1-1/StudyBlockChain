aeb1da5b5a1fdfdf7533cedf331ce115302eba66
fly-1-1 <2673024411@qq.com>
2023-11-13 04:24:28 +0800

    命令修改

--- BlockChain/cil.go
+++ BlockChain/cil.go
@@ -6,77 +6,63 @@ import (
 	"strconv"
 )
 
-//接收命令行参数&控制区块链操作的文件
+//这是一个用来接收命令行参数并且控制区块链操作的文件
 
 type CLI struct {
 	bc *BlockChain
 }
 
 const Usage = `
-	addBlock --data DATA  "add data to blockchain"
-	printChain			  "print all blockchain data"
-	printChainR			  "print all blockchain data revers"
-	getBalance --address ADDRESS      "get balance of address"
-	send FROM TO AMOUNT MINER DATA    "由FROM转AMOUNT给TO,由MINER挖矿,同时写入DATA"
+	printChain               "正向打印区块链"
+	printChainR              "反向打印区块链"
+	getBalance --address ADDRESS "获取指定地址的余额"
+	send FROM TO AMOUNT MINER DATA "由FROM转AMOUNT给TO，由MINER挖矿，同时写入DATA"
 `
 
-const addCil = "addBlock"
-const printCil = "printChain"
-const rprintCil = "printChainR"
-const getbalaceCil = "getBalance"
-const sendCil = "send"
+//接受参数的动作，我们放到一个函数中
 
-// Run 接收参数的动作
-func (cil *CLI) Run() {
-	//得到所有的参数
+func (cli *CLI) Run() {
+
+	//./block printChain
+	//./block addBlock --data "HelloWorld"
+	//1. 得到所有的命令
 	args := os.Args
 	if len(args) < 2 {
-		fmt.Println("cmd:--------------", args[1])
-		fmt.Printf("参数过少:%s", Usage)
+		fmt.Printf(Usage)
 		return
 	}
-	//分析命令
+
+	//2. 分析命令
 	cmd := args[1]
 	switch cmd {
-	case addCil:
-		fmt.Print("添加区块\n")
-		if len(args) == 4 && args[2] == "--data" {
-			//a 获取数据
-			data := args[3]
-			//b 使用bc添加区块
-			cil.AddBlock(data)
-		} else {
-			fmt.Println("添加区块参数有误")
-			fmt.Printf(Usage)
-		}
-	case printCil:
+	case "printChain":
 		fmt.Printf("正向打印区块\n")
-		cil.PrintChain()
-	case rprintCil:
+		cli.PrinBlockChain()
+	case "printChainR":
 		fmt.Printf("反向打印区块\n")
-		cil.PrintBlockChainRevers()
-	case getbalaceCil:
+		cli.PrinBlockChainReverse()
+	case "getBalance":
 		fmt.Printf("获取余额\n")
 		if len(args) == 4 && args[2] == "--address" {
 			address := args[3]
-			cil.GetBalance(address)
+			cli.GetBalance(address)
 		}
-	case sendCil:
+	case "send":
 		fmt.Printf("转账开始...\n")
 		if len(args) != 7 {
-			fmt.Println("参数个数错误")
+			fmt.Printf("参数个数错误，请检查！\n")
 			fmt.Printf(Usage)
 			return
 		}
+		//./block send FROM TO AMOUNT MINER DATA "由FROM转AMOUNT给TO，由MINER挖矿，同时写入DATA"
 		from := args[2]
 		to := args[3]
-		amount, _ := strconv.ParseFloat(args[4], 64)
+		amount, _ := strconv.ParseFloat(args[4], 64) //知识点，请注意
 		miner := args[5]
 		data := args[6]
-		cil.Send(from, to, amount, miner, data)
+		cli.Send(from, to, amount, miner, data)
 	default:
-		fmt.Print("无效的命令\n")
+		fmt.Printf("无效的命令，请检查!\n")
 		fmt.Printf(Usage)
 	}
-	//执行相应动作
 }
