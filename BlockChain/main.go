7ff0f22dc178756b7441bf6b9caf431529091c4a
fly-1-1 <2673024411@qq.com>
2023-11-13 04:25:16 +0800

    无

--- BlockChain/main.go
+++ BlockChain/main.go
@@ -1,26 +1,9 @@
 package main
 
+//import "fmt"
+
 func main() {
-
-	//block := NewBlock("A->B 1 BTC", []byte{})
-
-	bc := NewBlockChain("333")
-	cil := CLI{bc}
-	cil.Run()
-
-	//bc.AddBlock("A->B 20 BTC")
-	//bc.AddBlock("A->B 30 BTC")
-	//it := bc.NewIterator()
-	////调用迭代器 返回每一个区块数据
-	//for {
-	//	//返回区块 左移
-	//	block := it.Next()
-	//	fmt.Printf("=========================================\n\n")
-	//	fmt.Printf("前区块Hash:%x\n", block.PrevHash)
-	//	fmt.Printf("当前区块Hash:%x\n", block.Hash)
-	//	fmt.Printf("区块数据:%s\n", block.Data)
-	//	if len(block.PrevHash) == 0 {
-	//		break
-	//	}
-	//}
+	bc := NewBlockChain("张三")
+	cli := CLI{bc}
+	cli.Run()
 }
