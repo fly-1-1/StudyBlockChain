ca7e16180503d79d06df4e7defde13183b797786
fly-1-1 <2673024411@qq.com>
2023-11-14 04:40:06 +0800

    checksum校验

--- BlockChain/wallet.go
+++ BlockChain/wallet.go
@@ -1,68 +1,110 @@
 package main
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
+	"fmt"
 	"github.com/btcsuite/btcutil/base58"
 	"golang.org/x/crypto/ripemd160"
 	"log"
 )
 
 //钱包结构 每一个钱包保存了公钥私钥对
-
 type Wallet struct {
+	//私钥
 	Private *ecdsa.PrivateKey
-	//PubKey不存储原始公钥 而是存储原始的x和y拼接的字符串 在校验端重新拆分
-	PubKey []byte
+	//PubKey *ecdsa.PublicKey
+	//约定，这里的PubKey不存储原始的公钥，而是存储X和Y拼接的字符串，在校验端重新拆分（参考r,s传递）
+	PubKey []byte //
 }
 
-// NewWallet 创建钱包
+//创建钱包
 func NewWallet() *Wallet {
-	Curve := elliptic.P256()
-	privateKey, err := ecdsa.GenerateKey(Curve, rand.Reader)
+	//创建曲线
+	curve := elliptic.P256()
+	//生成私钥
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
 	if err != nil {
-		log.Panic(err)
+		log.Panic()
 	}
+
 	//生成公钥
 	pubKeyOrig := privateKey.PublicKey
+
+	//拼接X, Y
 	pubKey := append(pubKeyOrig.X.Bytes(), pubKeyOrig.Y.Bytes()...)
+
 	return &Wallet{Private: privateKey, PubKey: pubKey}
 }
 
-// NewAddress 生成地址
+//生成地址
 func (w *Wallet) NewAddress() string {
 	pubKey := w.PubKey
-	rip160hashValue := HashPubKey(pubKey)
+
+	rip160HashValue := HashPubKey(pubKey)
 	version := byte(00)
-	payload := append([]byte{version}, rip160hashValue...)
+	//拼接version
+	payload := append([]byte{version}, rip160HashValue...)
+
+	//checksum
 	checkCode := CheckSum(payload)
+
 	//25字节数据
 	payload = append(payload, checkCode...)
-	//btcd BTC全节点源码
+
+	//go语言有一个库，叫做btcd,这个是go语言实现的比特币全节点源码
 	address := base58.Encode(payload)
+
 	return address
 }
 
 func HashPubKey(data []byte) []byte {
 	hash := sha256.Sum256(data)
-	//编码器
 
+	//理解为编码器
 	rip160hasher := ripemd160.New()
 	_, err := rip160hasher.Write(hash[:])
+
 	if err != nil {
 		log.Panic(err)
 	}
-	rip160hashValue := rip160hasher.Sum(nil)
-	return rip160hashValue
+
+	//返回rip160的哈希结果
+	rip160HashValue := rip160hasher.Sum(nil)
+	return rip160HashValue
 }
 
 func CheckSum(data []byte) []byte {
-	//checksum
+	//两次sha256
 	hash1 := sha256.Sum256(data)
 	hash2 := sha256.Sum256(hash1[:])
-	//字节校验码
+
+	//前4字节校验码
 	checkCode := hash2[:4]
 	return checkCode
+}
+
+func IsValidAddress(address string) bool {
+	//1. 解码
+	addressByte := base58.Decode(address)
+
+	if len(addressByte) < 4 {
+		return false
+	}
+
+	//2. 取数据
+	payload := addressByte[:len(addressByte)-4]
+	checksum1 := addressByte[len(addressByte)-4:]
+
+	//3. 做checksum函数
+	checksum2 := CheckSum(payload)
+
+	fmt.Printf("checksum1 : %x\n", checksum1)
+	fmt.Printf("checksum2 : %x\n", checksum2)
+
+	//4. 比较
+	return bytes.Equal(checksum1, checksum2)
 }
