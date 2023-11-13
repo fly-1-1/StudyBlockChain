29248d7fa779890c5ba3339e8626626df5565a5c
fly-1-1 <2673024411@qq.com>
2023-11-14 04:41:41 +0800

    命令修改

--- BlockChain/commandLine.go
+++ BlockChain/commandLine.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//"time"
 )
 
-// PrintBlockChain 正向打印
-func (cli *CLI) PrintBlockChain() {
-	cli.bc.PrintChain()
+//正向打印
+func (cli *CLI) PrinBlockChain() {
+	cli.bc.Printchain()
 	fmt.Printf("打印区块链完成\n")
 }
 
-// PrintBlockChainReverse PrinBlockChainReverse 反向打印
-func (cli *CLI) PrintBlockChainReverse() {
+//反向打印
+func (cli *CLI) PrinBlockChainReverse() {
 	bc := cli.bc
 	//创建迭代器
 	it := bc.NewIterator()
@@ -22,16 +22,21 @@ func (cli *CLI) PrintBlockChainReverse() {
 		//返回区块，左移
 		block := it.Next()
 
-		fmt.Printf("===========================\n\n")
-		fmt.Printf("版本号: %d\n", block.Version)
-		fmt.Printf("前区块哈希值: %x\n", block.PrevHash)
-		fmt.Printf("梅克尔根: %x\n", block.MerkelRoot)
-		timeFormat := time.Unix(int64(block.TimeStamp), 0).Format("2006-01-02 15:04:05")
-		fmt.Printf("时间戳: %s\n", timeFormat)
-		fmt.Printf("难度值(随便写的）: %d\n", block.Difficulty)
-		fmt.Printf("随机数 : %d\n", block.Nonce)
-		fmt.Printf("当前区块哈希值: %x\n", block.Hash)
-		fmt.Printf("区块数据 :%s\n", block.Transactions[0].TXInputs[0].Sig)
+		for _, tx := range block.Transactions {
+			fmt.Println(tx)
+		}
+		/*
+			fmt.Printf("===========================\n\n")
+			fmt.Printf("版本号: %d\n", block.Version)
+			fmt.Printf("前区块哈希值: %x\n", block.PrevHash)
+			fmt.Printf("梅克尔根: %x\n", block.MerkelRoot)
+			timeFormat := time.Unix(int64(block.TimeStamp), 0).Format("2006-01-02 15:04:05")
+			fmt.Printf("时间戳: %s\n", timeFormat)
+			fmt.Printf("难度值(随便写的）: %d\n", block.Difficulty)
+			fmt.Printf("随机数 : %d\n", block.Nonce)
+			fmt.Printf("当前区块哈希值: %x\n", block.Hash)
+			fmt.Printf("区块数据 :%s\n", block.Transactions[0].TXInputs[0].PubKey)
+		*/
 
 		if len(block.PrevHash) == 0 {
 			fmt.Printf("区块链遍历结束！")
@@ -42,13 +47,20 @@ func (cli *CLI) PrintBlockChainReverse() {
 
 func (cli *CLI) GetBalance(address string) {
 
-	utxos := cli.bc.FindUTXOs(address)
+	//1. 校验地址
+	if !IsValidAddress(address) {
+		fmt.Printf("地址无效 : %s\n", address)
+		return
+	}
+	//2. 生成公钥哈希
+	pubKeyHash := GetPubKeyFromAddress(address)
+
+	utxos := cli.bc.FindUTXOs(pubKeyHash)
 
 	total := 0.0
 	for _, utxo := range utxos {
 		total += utxo.Value
 	}
-
 	fmt.Printf("\"%s\"的余额为：%f\n", address, total)
 }
 
@@ -58,6 +70,19 @@ func (cli *CLI) Send(from, to string, amount float64, miner, data string) {
 	//fmt.Printf("amount : %f\n", amount)
 	//fmt.Printf("miner : %s\n", miner)
 	//fmt.Printf("data : %s\n", data)
+	//1. 校验地址
+	if !IsValidAddress(from) {
+		fmt.Printf("地址无效 from: %s\n", from)
+		return
+	}
+	if !IsValidAddress(to) {
+		fmt.Printf("地址无效 to: %s\n", to)
+		return
+	}
+	if !IsValidAddress(miner) {
+		fmt.Printf("地址无效 miner: %s\n", miner)
+		return
+	}
 
 	//1. 创建挖矿交易
 	coinbase := NewCoinbaseTX(miner, data)
@@ -76,13 +101,14 @@ func (cli *CLI) Send(from, to string, amount float64, miner, data string) {
 func (cli *CLI) NewWallet() {
 	ws := NewWallets()
 	address := ws.CreateWallet()
-	fmt.Printf("地址 %s\n", address)
+	fmt.Printf("地址：%s\n", address)
+
 }
 
-func (cli *CLI) listAddresses() {
+func (cli *CLI) ListAddresses() {
 	ws := NewWallets()
 	addresses := ws.ListAllAddresses()
 	for _, address := range addresses {
-		fmt.Printf("地址 %s\n", address)
+		fmt.Printf("地址：%s\n", address)
 	}
 }
